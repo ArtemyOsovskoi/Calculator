747a0b5dffb3309138f17ce25c30f75f75df8813
Artemy <116560582+ArtemyOsovskoi@users.noreply.github.com>
2023-06-30 10:16:05 +0300

    reworked fillDisplay function: first number gets updated until user pressed equals button

--- script.js
+++ script.js
@@ -42,33 +42,54 @@ function fillDisplay () {
             if (displayLength.length < 55 && operator === '') { //set limit on a numbers entered
                 getInputDisplay.innerText += num.value; 
                 a += event.target.value;
-                //console.log(a);
+                //prevent '.' double clicking
+                let displayToArray = getInputDisplay.innerText.split('');
+                let lastChar = displayToArray[displayToArray.length - 1];
+                if (lastChar === '.') {
+                    document.getElementById('.').disabled = true;
+                }
+                console.log(a);
             } else if (operator === '+' || operator === '-'|| operator === '*' || operator === '/') {
-                getInputDisplay.innerText += num.value; 
+                getInputDisplay.innerText += num.value;
+                //if (b == '') {b = event.target.value;}
                 b += event.target.value;
-                getResultDisplay.innerText = calculator(a, operator, b);
+                //getResultDisplay.innerText = calculator(a, operator, b);
+                //prevent '.' double clicking
+                let displayToArray = getInputDisplay.innerText.split('');
+                let lastChar = displayToArray[displayToArray.length - 1];
+                if (lastChar === '.') {
+                    document.getElementById('.').disable = true;
+                }
+                //prevent from dividing by 0
                 if (getInputDisplay.innerHTML.match(/[/]/g) && b === '0') {
                     b = ''
-                    //getInputDisplay.innerText = getInputDisplay.innerText.slice(0, -1); 
-                    getResultDisplay.innerText = 'Can\'t divide by 0';
+                    getInputDisplay.innerText = getInputDisplay.innerText.slice(0, -1); 
+                    alert('Can\'t divide by 0');
                 }
                 //console.log(getInputDisplay.innerHTML.match(/[/]/g) > 1);
-                //console.log(b);
+                console.log(b);
             }  
         })
     })
     Array.from(mathOpButtons).forEach((math) => { 
         math.addEventListener('click', (event) => {
             getInputDisplay.innerText += math.value;
-            operator = event.target.innerText;
-            if (getInputDisplay.innerHTML.match(/[\/\+\-\*]/g).length > 1) { //check if display has more than 1 math operator
-                a = getResultDisplay.innerText;
+            if (getInputDisplay.innerHTML.match(/[\/\+\-\*]/g).length > 1) { 
+                //a = getResultDisplay.innerText;
+                a = calculator(a, operator, b);
+                operator = event.target.innerText;
+                getInputDisplay.innerText = a + operator;
             } 
+            operator = event.target.innerText;
             b = ''
+            //check if display has more than 1 math operator
+
+            //b = ''
+        console.log(operator);
         })
     })
     equalsButton.addEventListener('click', () => {
-        getResultDisplay.innerText = calculator(a, operator, b);
+        getResultDisplay.innerText = calculator(a, operator, b).toFixed(2);
         getInputDisplay.innerHTML = ''
         a = ''
         b = ''
@@ -80,7 +101,7 @@ fillDisplay();
 
 //calculator function
 function calculator (a, operator, b) {
-    return operate(operator, Number(a), Number(b)).toFixed(2)
+    return operate(operator, Number(a), Number(b))
 }
 
 //clear function
@@ -98,33 +119,52 @@ let deleteButton = document.getElementById('delete');
 deleteButton.addEventListener('click', ()=> {
     let displayToArray = getInputDisplay.innerText.split('');
     let lastChar = displayToArray[displayToArray.length - 1];
+    //1.когда 250+50 - lastChar = number, operator !='', b != ''
+    if (lastChar.match(/^(\d)*(\.)?([0-9]{1})?$/gm) && operator != '' && b.length >= 1) {
+        //когда 250+50+5 
+        if (b.length == 1 && getInputDisplay.innerHTML.match(/[\/\+\-\*]/g).length > 1) {
+            b = b.slice(0, -1);
+            getInputDisplay.innerText = getInputDisplay.innerText.slice(0, -1);
+            getResultDisplay.innerText = Number(a);
+        }
+        //когда 250+50 
+        if (b.length > 1) {
+            b = b.slice(0, -1);
+            getResultDisplay.innerText = calculator (a, operator, b);
+            getInputDisplay.innerText = getInputDisplay.innerText.slice(0, -1);
+        }  
+    }
 
-    if (lastChar == '+' || lastChar == '-' ||lastChar == '*' || lastChar == '/') {
+   /*  //2.когда 250+5 - lastChar=number, operator != '', b = ''
+    if (lastChar.match(/^(\d)*(\.)?([0-9]{1})?$/gm) && operator !='' && b == '') {
+        //b = b.slice(0, -1);
+        getInputDisplay.innerText = getInputDisplay.innerText.slice(0, -1);
+        getResultDisplay.innerText = getResultDisplay.innerText.slice(0, -1);
+    } */
+
+    //3. когда 250+ - lastChar = operator, operator = '', b = ''\b != '' (250+25-2)
+    if (lastChar.match(/[\/\+\-\*]/g) && b == '' ) {
+        getInputDisplay.innerText = getInputDisplay.innerText.slice(0, -1);
+        //getResultDisplay.innerText = getResultDisplay.innerText.slice(0, -1);
+        //a = a.slice(0, -1);
         operator = ''
-        getInputDisplay.innerText = getInputDisplay.innerText.slice(0, -1);
-        getResultDisplay.innerText = calculator(a, operator, b);
+        
+        //когда 250+50-
+        if (b != '' ) {
+            getInputDisplay.innerText = getInputDisplay.innerText.slice(0, -1);
+            //getResultDisplay.innerText = getResultDisplay.innerText.slice(0, -1);
+            operator = '';
+        }
     }
-    if (lastChar.match(/^\d+$/)){
-        b = b.slice(0, -1);
+    
+    //4. когда 250 - lastChar = number, operator = '', b = ''
+    if (lastChar.match(/^(\d)*(\.)?([0-9]{1})?$/gm) && operator == '' && b == '') {
         getInputDisplay.innerText = getInputDisplay.innerText.slice(0, -1);
-        getResultDisplay.innerText = calculator(a, operator, b);
+        getResultDisplay.innerText = getResultDisplay.innerText.slice(0, -1);
+        a = a.slice(0, -1);
     }
-     /* if (последний символ это математич операция) {operator=''} 
-    if (последний символ это число b) {b=''} */
 })
-/* 
-23.06 что поменять:
-- нужно считать результат каждых ДВУХ цифр, т.е. когда вводим 1+ 2+ 3, 1+2 считаются в момент ввода 2 и 
-после при вводе 3 складывается получая 6;
 
-
- */
-/* Users should be able to string together several operations and get the right answer, 
-with each pair of numbers being evaluated at a time. For example, 12 + 7 - 5 * 3 = should yield 42. 
-
-Your calculator SHOULD NOT EVALUATE MORE THAN A SINGLE PAIR of numbers AT A TIME. 
-Example: you press a number button (12), followed by an operator button (+), a second number button (7), 
-and finally a second operator button (-). Your calculator should then do the following: 
-- first, evaluate the first pair of numbers (12 + 7), 
-- second, display the result of that calculation (19), 
-- and finally, use that result (19) as the first number in your new calculation,along with the next operator (-). */
+/* 29.06 - снова запутался, переписав функцию, проблема в ситуации 250+50+5 когда 5 = b удаляем
+и непонятно как "сделать" 50 в b тк иначе при 250+50 b == 0 или (если не удалять) или b == 5 что в обоих
+случаях неверно */
